e534a9ae1626f88c1e67d6aa292e60c9790291d3
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-07-09 15:56:41 +0700

    connect to db mysql

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.14.0
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.21.11 // indirect
 )
--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("root:root@tpc(db:3306)/ambassador"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to database")
+	}
 	app := fiber.New()
 
 	// Default middleware config
