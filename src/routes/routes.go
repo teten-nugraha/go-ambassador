36b99edc9b10a0e6b336541b39f9c19067bbfdc9
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-07-12 12:04:35 +0700

    update logout url

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -13,5 +13,5 @@ func Setup(app *fiber.App) {
 	admin.Post("/register", controllers.Register)
 	admin.Post("/login", controllers.Login)
 	admin.Get("/user", controllers.User)
-	admin.Get("/logout", controllers.Logout)
+	admin.Post("/logout", controllers.Logout)
 }
