7780c2aa78753960f5013ca51e9163db0b21a3a9
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-07-12 16:51:43 +0700

    products crud

---
+++ src/controllers/productController.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+	"github.com/gofiber/fiber/v2"
+	"strconv"
+)
+
+func Products(c *fiber.Ctx) error {
+	var products []models.Product
+
+	database.DB.Find(&products)
+
+	return c.JSON(products)
+}
+
+func CreateProducts(c *fiber.Ctx) error {
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Create(&product)
+
+	return c.JSON(product)
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	var product models.Product
+
+	product.Id = uint(id)
+
+	database.DB.Find(&product)
+
+	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{
+		Id:          uint(id),
+	}
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Model(&product).Updates(&product)
+
+	return c.JSON(product)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{
+		Id:          uint(id),
+	}
+
+	database.DB.Delete(&product)
+
+	return c.JSON(fiber.Map{
+		"message": "product success deleted",
+	})
+}
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -15,10 +15,19 @@ func Setup(app *fiber.App) {
 	admin.Post("/login", controllers.Login)
 
 	adminAuthenticated := admin.Use(middlewares.IsAuthenticated)
+
+	//Users
 	adminAuthenticated.Get("/user", controllers.User)
 	adminAuthenticated.Post("/logout", controllers.Logout)
 	adminAuthenticated.Post("/updateInfo", controllers.UpdateInfo)
 	adminAuthenticated.Post("/updatePassword", controllers.UpdatePassword)
 	adminAuthenticated.Get("/ambassadors", controllers.Ambassadors)
 	adminAuthenticated.Get("/populateAmbassadors", controllers.PopulateAmbassadors)
+
+	// Products
+	adminAuthenticated.Get("/products", controllers.Products)
+	adminAuthenticated.Post("/products", controllers.CreateProducts)
+	adminAuthenticated.Get("/products/:id", controllers.GetProduct)
+	adminAuthenticated.Put("/products/:id", controllers.UpdateProduct)
+	adminAuthenticated.Delete("/products/:id", controllers.DeleteProduct)
 }
