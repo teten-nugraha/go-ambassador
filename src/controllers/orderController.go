fcf1cef787e5652f2b5ddb4ecbf2ebcd2a4f2dbd
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-07-19 13:05:58 +0700

    Update links and orders

--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -13,6 +13,11 @@ func Orders(c *fiber.Ctx) error {
 
 	database.DB.Preload("OrderItems").Find(&orders)
 
+	for i, order := range orders{
+		orders[i].Name = order.FullName()
+		orders[i].Total = order.GetTotal()
+	}
+
 	return c.JSON(orders)
 }
 
