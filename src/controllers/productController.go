d47170b0fe1ee7e6cf4f8e3c5cc0a8b6a3bdd8dd
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-08-19 11:57:26 +0700

    gorountine

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -58,8 +58,17 @@ func UpdateProduct(c *fiber.Ctx) error {
 	}
 
 	database.DB.Model(&product).Updates(&product)
+	
+	go deleteCache("products_frontend")
+	go deleteCache("products_backend")
 
 	return c.JSON(product)
+}
+
+func deleteCache(key string) {
+	time.Sleep(3 * time.Second)
+
+	database.Cache.Del(context.Background(), key)
 }
 
 func DeleteProduct(c *fiber.Ctx) error {
